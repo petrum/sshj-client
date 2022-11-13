63f6c209499ac5a8543ce1a4c9a454bbeba6ed2b
petrum <petru.marginean@gmail.com>
2022-11-13 14:18:15 -0500

    misc

--- rerun.sh
+++ rerun.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm ~/.ssh/id_rsa*
-~/sshj-client/run.sh
-~/sshj-client/deploykey.sh
-ssh www.petrum.net -p 22223 uname
-~/sshj-client/run.sh
-~/sshj-client/run.sh /home/petrum/.ssh/bak/id_rsa
-cp -p ~/.ssh/bak/id* ~/.ssh
+rm ~/.ssh/id_rsa* # it removes the keys so they'll be recreated
+~/sshj-client/run.sh /home/petrum/.ssh/id_rsa # this will just regenerate new keys as they are missing
+~/sshj-client/deploykey.sh # it appends the new pub key to the remote server
+ssh www.petrum.net -p 22223 uname # it checks it works with ssh
+~/sshj-client/run.sh /home/petrum/.ssh/id_rsa # it uses the keys from exiting files
+~/sshj-client/run.sh /home/petrum/.ssh/bak/id_rsa # it uses the keys from openssh backup files
+cp -p ~/.ssh/bak/id* ~/.ssh # it restores the backup files for normal usage
--- run.sh
+++ run.sh
@@ -2,15 +2,7 @@
 
 SRC="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $SRC
-
 JAR=out/artifacts/sshj_client_main_jar/sshj-client.main.jar
 zip -d $JAR 'META-INF/*SF'
-#KEY=/mnt/c/Users/petru/.ssh/id_rsa
-#if [[ ! -f $KEY ]]; then
-#    KEY=/home/petrum/.ssh/id_rsa
-#fi
-KEY=~/.ssh/id_rsa
-if [[ $1 != '' ]]; then
-  KEY=$1
-fi
+KEY=$1
 java -Dkotlin-logging.throwOnMessageError -cp $JAR MainKt www.petrum.net 22223 petrum $KEY 'uname -a'
