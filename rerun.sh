fab5767dc632c1ae8df833ebd15e98d555f2f1cb
petrum <petru.marginean@gmail.com>
2022-11-13 15:23:47 -0500

    misc

--- rerun.sh
+++ rerun.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 rm ~/.ssh/id_rsa* # it removes the keys so they'll be recreated
-~/sshj-client/run.sh /home/petrum/.ssh/id_rsa # this will just regenerate new keys as they are missing
-~/sshj-client/deploykey.sh # it appends the new pub key to the remote server
-ssh www.petrum.net -p 22223 uname # it checks it works with ssh
-~/sshj-client/run.sh /home/petrum/.ssh/id_rsa # it uses the keys from exiting files
-~/sshj-client/run.sh /home/petrum/.ssh/bak/id_rsa # it uses the keys from openssh backup files
-cp -p ~/.ssh/bak/id* ~/.ssh # it restores the backup files for normal usage
+echo - this will just regenerate new keys as they are missing...
+~/sshj-client/run.sh /home/petrum/.ssh/id_rsa
+echo - it appends the new pub key to the remote server...
+~/sshj-client/deploykey.sh
+echo - it checks if new sshj created keys work with ssh...
+ssh www.petrum.net -p 22223 uname
+echo - it uses the new keys from new created files...
+~/sshj-client/run.sh /home/petrum/.ssh/id_rsa
+echo - it uses the keys from openssh backup files...
+~/sshj-client/run.sh /home/petrum/.ssh/bak/id_rsa
+echo - it restores the backup files for normal usage...
+cp -p ~/.ssh/bak/id* ~/.ssh
 echo Done
