f51e5434ad0a4a30816d5c16d396e15ecf27d8fa
petrum <petru.marginean@gmail.com>
2022-11-13 13:06:37 -0500

    misc

--- deploykey.sh
+++ deploykey.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ssh -i ~/.ssh/bak/id_rsa www.petrum.net -p 22223 "grep -v sshj-client2 .ssh/authorized_keys > .ssh/authorized_keys2; mv .ssh/authorized_keys2 .ssh/authorized_keys"
-cat ~/.ssh/id_rsa.pub | ssh -i ~/.ssh/bak/id_rsa www.petrum.net -p 22223 tee -a .ssh/authorized_keys
+cat ~/.ssh/id_rsa.pub | ssh -i ~/.ssh/bak/id_rsa www.petrum.net -p 22223 "cat >> .ssh/authorized_keys"
 chmod 600 ~/.ssh/id_rsa*
